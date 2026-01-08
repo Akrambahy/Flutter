59b5693f68fd25c647faeb809374fdc34017bb39
AkramBahy <akrambahy0@gmail.com>
2026-01-08 07:32:06 +0200

    Add new lectures

---
+++ Dart/DataTypes.dart
@@ -0,0 +1,19 @@
+void main() {
+  int inumber = 6; // int is Data type of number and digit
+  double dNumber = 6.62; // double is Data type of number and digit but are floting like 5.20 
+  num number = 6.62; // num is Data type of number and digit both real number are floting like 5.20  or 5 
+ 
+ 
+  String text ="text massage"; // String is Data type of text, Note : S in String is capital
+ 
+ 
+ bool isOnline=true; // bool is Data Type for binary Questions just have only tow values {true,false}
+ 
+ dynamic dyVar='dynamic';// dynamic is a data type dynamic can store any data  text number boll we used when dont know out type data
+ 
+  print(text);
+  print("new massage : $text", ); // we use $ before variable name to mark it as a variable
+  print("new massage : \t", ); // \t her is mean is a '\t' as normal text by using r before text r mean is text are row text
+  print("new massage \t"); // \t her is mean a tap like ' '
+
+}
