8a4659e2a8552bf1ac700b8b0f877a9b814e382c
AkramBahy <akrambahy0@gmail.com>
2026-02-02 03:53:28 +0200

    add : Encapsulation

---
+++ Dart/OOP/ClassAndObject/main.dart
@@ -0,0 +1,11 @@
+import "Human.dart";
+
+void main(){
+
+Human h=Human(name:"Akram",age:19);
+h.age=20;
+print(h.name);
+}
+
+
+
---
+++ Dart/OOP/Encapsulation/Human.dart
@@ -0,0 +1,31 @@
+class Human{
+String? _name; // _ before var name make it private just fore this file
+int? _age;
+
+
+void setName(String name){
+this._name=name;
+}
+int? getAge(){
+return this._age;
+}
+
+Human({String ?name , int ?age}) //parameterized constructor
+{
+this._name=name;
+this._age=age;
+}
+
+// arrow function we is it if implementation on one line
+//example
+void printHi()=>print("Hi");
+// Advance Setter And Getter
+
+set age(int? age){ //  have't return type
+if(age!=null) this._age=age;
+else this._age=0;
+}
+
+String? get name=> _name; // have't parameters
+
+}
