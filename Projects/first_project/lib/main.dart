2eda1237488163ef3d6f1a38cff76bfdee242dc1
AkramBahy <akrambahy0@gmail.com>
2026-02-09 08:46:25 +0200

    add : new topic

--- Projects/first_project/lib/main.dart
+++ Projects/first_project/lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
+  runApp(OnApp());
+}
 
-runApp(
-
-MaterialApp(
-home : Text(
-"Hi, I'm "
-) 
-
-)
-);
+class OnApp extends StatelessWidget {
+  OnApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(width:500,height:500,color:Colors.black12,child: Center(child: Text("Akram"),),),
+            Container(width:500,height:500,color:Colors.white12,child: Center(child: Text("Bahy"),),),
+          ],
+        ),
+      ),
+    );
+  }
 }
